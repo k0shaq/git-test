7fa005fcc67a8add33e29c4c8b0f50e82692e841
Sanek <sanik.voronin@gmail.com>
2024-04-19 15:28:01 +0300

    Testing map

--- new project/new project.cpp
+++ new project/new project.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <set>
+#include <map>
 
 template<typename T>
 void showSet(const std::set<T>& abc);
+
+template<typename T>
+void showMap(const std::map<int, T>& abc);
+
 
 int main()
 {
@@ -14,7 +19,20 @@ int main()
 	mySet.insert(2);
 	showSet(mySet);
 	std::cout << std::endl;
-	std::cout << mySet.size();
+
+	std::map<int, std::string> myMap;
+	std::map<int, std::string> newMap;
+	myMap[1] = "Kota";
+	myMap[2] = "Pesa";
+	myMap[4] = "Koza";
+	myMap[3] = "Cow";
+	showMap(myMap);
+	newMap[21] = "Lincolkn";
+	newMap[2] = "Mercedes";
+	newMap[-1] = "bmw";
+	showMap(newMap);
+	myMap.swap(newMap);
+	showMap(myMap);
 }
 
 template<typename T>
@@ -23,4 +41,13 @@ void showSet(const std::set<T>& abc) {
 	{
 		std::cout << *it;
 	}
+}
+
+template<typename T>
+void showMap(const std::map<int, T>& abc) {
+	for (auto it = abc.begin(); it != abc.end(); ++it)
+	{
+		std::cout << it->first << ": " << it->second << " ";
+	}
+	std::cout << std::endl;
 }
