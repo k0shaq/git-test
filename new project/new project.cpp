51dd58d5cd5e6b07d8bdfa29a8a7486496d55f49
Sanek <sanik.voronin@gmail.com>
2024-04-22 11:32:41 +0300

    NOT WORK: random access iterator

--- new project/new project.cpp
+++ new project/new project.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <iterator>
 #include <list>
+#include <vector>
 
 int main() {
-    std::list<int> nums = { 1, 2, 3, 4 };
-    std::list<int>::iterator itr;
+    std::vector<int> nums = { 1, 2, 3, 4, 5, 6 };
+    std::vector<int>::iterator itr;
     for (itr = nums.begin(); itr != nums.end(); ++itr) {
         std::cout << *itr << " ";
     }
-    for (--itr; itr != nums.begin(); --itr) {
+    for (itr -= 1; itr != nums.begin() - 1; --itr) {
         std::cout << *itr << " ";
     }
 }
