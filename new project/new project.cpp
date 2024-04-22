705f51564a52fd24597027373285236b365edaf8
Sanek <sanik.voronin@gmail.com>
2024-04-22 10:42:09 +0300

    output iterator

--- new project/new project.cpp
+++ new project/new project.cpp
@@ -3,14 +3,9 @@
 #include <vector>
 
 int main() {
-    std::vector<int> nums = { 1, 2, 3, 4, 5, 6};
-    std::istream_iterator<int> input(std::cin);
-    std::copy(input, std::istream_iterator<int>(), std::back_inserter(nums));
-
-    for (int num : nums) {
-        std::cout << num << " ";
-    }
-    std::cout << std::endl;
+    std::vector<int> nums = { 1, 2, 3, 4, 5 };
+    std::ostream_iterator<int> output(std::cout, " ");
+    std::copy(nums.begin(), nums.end(), output);
 
     return 0;
 }
