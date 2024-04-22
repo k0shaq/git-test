1bcf6b994b8f1ea31294b56e6798ebfe15d6dc0c
Sanek <sanik.voronin@gmail.com>
2024-04-22 10:41:29 +0300

    input iterator

--- new project/new project.cpp
+++ new project/new project.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 int main() {
-    std::vector<int> nums = { 1, 2, 3, 4, 5 };
+    std::vector<int> nums = { 1, 2, 3, 4, 5, 6};
     std::istream_iterator<int> input(std::cin);
     std::copy(input, std::istream_iterator<int>(), std::back_inserter(nums));
 
