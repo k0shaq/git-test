a296664e8acc8704f0ee8ef91a39a2d1800cd6b3
Sanek <sanik.voronin@gmail.com>
2024-04-22 11:56:06 +0300

    clear

--- new project/new project.cpp
+++ new project/new project.cpp
@@ -3,13 +3,6 @@
 #include <vector>
 
 int main() {
-    std::vector<int> nums = { 8, 3, 9, 4, 1, 5, 0, 9, 2, 6, 5, 3 };
-    std::partial_sort(nums.begin(), nums.begin() + 5, nums.end());
-
-    for (int num : nums) {
-        std::cout << num << " ";
-    }
-    std::cout << std::endl;
-
+    
     return 0;
 }
