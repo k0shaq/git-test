9e7166abe15d4833a251285c72712609e2c6cfdc
Sanek <sanik.voronin@gmail.com>
2024-04-22 11:55:52 +0300

    Partial sort

--- new project/new project.cpp
+++ new project/new project.cpp
@@ -1,7 +1,15 @@
+#include <iostream>
 #include <algorithm>
 #include <vector>
-#include <iostream>
 
 int main() {
+    std::vector<int> nums = { 8, 3, 9, 4, 1, 5, 0, 9, 2, 6, 5, 3 };
+    std::partial_sort(nums.begin(), nums.begin() + 5, nums.end());
 
+    for (int num : nums) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
