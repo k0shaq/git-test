fae2044dd204bfbde920931c413d2e215d02411c
Sanek <sanik.voronin@gmail.com>
2024-04-22 12:13:58 +0300

    Add ua lang

--- new project/new project.cpp
+++ new project/new project.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <Windows.h>
 
 int main() {
-    
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+   
+
+
     return 0;
 }
