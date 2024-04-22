c1d935439371cd1bf4d16b496ba5642bb6417a0b
Sanek <sanik.voronin@gmail.com>
2024-04-22 11:34:35 +0300

    clear

--- new project/new project.cpp
+++ new project/new project.cpp
@@ -3,11 +3,5 @@
 #include <iostream>
 
 int main() {
-    std::vector<int> nums = { 10, 9, 8, 7, 6, 5 };
-    std::sort(nums.begin(), nums.end());
 
-    for (int num : nums) {
-        std::cout << num << ' ';
-    }
-    // Output: 5 6 7 8 9 10
 }
