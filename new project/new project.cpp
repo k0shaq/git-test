843406b92d649a766d83342bac95b2bd9e546134
Sanek <sanik.voronin@gmail.com>
2024-04-22 10:31:33 +0300

    Testing iterators

--- new project/new project.cpp
+++ new project/new project.cpp
@@ -1,53 +1,16 @@
 #include <iostream>
-#include <set>
-#include <map>
+#include <iterator>
+#include <vector>
 
-template<typename T>
-void showSet(const std::set<T>& abc);
+int main() {
+    std::vector<int> nums = { 1, 2, 3, 4, 5 };
+    std::istream_iterator<int> input(std::cin);
+    std::copy(input, std::istream_iterator<int>(), std::back_inserter(nums));
 
-template<typename T>
-void showMap(const std::map<int, T>& abc);
+    for (int num : nums) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
 
-
-int main()
-{
-	std::set<int> mySet = { 1,3,4};
-	showSet(mySet);
-	std::cout << std::endl;
-	auto it = mySet.find(3);
-	mySet.erase(it);
-	mySet.insert(2);
-	showSet(mySet);
-	std::cout << std::endl;
-
-	std::map<int, std::string> myMap;
-	std::map<int, std::string> newMap;
-	myMap[1] = "Kota";
-	myMap[2] = "Pesa";
-	myMap[4] = "Koza";
-	myMap[3] = "Cow";
-	showMap(myMap);
-	newMap[21] = "Lincolkn";
-	newMap[2] = "Mercedes";
-	newMap[-1] = "bmw";
-	showMap(newMap);
-	myMap.swap(newMap);
-	showMap(myMap);
-}
-
-template<typename T>
-void showSet(const std::set<T>& abc) {
-	for (auto it = abc.begin(); it != abc.end(); ++it)
-	{
-		std::cout << *it;
-	}
-}
-
-template<typename T>
-void showMap(const std::map<int, T>& abc) {
-	for (auto it = abc.begin(); it != abc.end(); ++it)
-	{
-		std::cout << it->first << ": " << it->second << " ";
-	}
-	std::cout << std::endl;
+    return 0;
 }
