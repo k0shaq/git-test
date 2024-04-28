8c709098106a91c5e6ba41a387801a9fdd9faa48
Sanek <sanik.voronin@gmail.com>
2024-04-28 15:25:36 +0300

    Upload 28.04

--- new project/new project.cpp
+++ new project/new project.cpp
@@ -6,8 +6,17 @@
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-   
+    std::vector<int> nums1 = { 1, 3, 5, 7 };
+    std::vector<int> nums2 = { 2, 4, 6, 8 };
+    std::vector<int> merged;
 
+    std::merge(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), std::back_inserter(merged));
+
+    for (int num : merged) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
-}
+    }
+
